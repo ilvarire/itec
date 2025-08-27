00be72c3a5d9f27daf81415c2fed5fb9d210ce01
Your Name <you@example.com>
2025-08-27 10:48:56 -0700

    ch

--- resources/views/components/layouts/main.blade.php
+++ resources/views/components/layouts/main.blade.php
@@ -142,7 +142,19 @@
     <script src="{{ url('/assets/plugins/plugins.js')}}"></script>
     <script src="{{ url('/assets/functions.js')}}"></script>
     <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
-
+    <!--Start of Tawk.to Script-->
+    <script type="text/javascript">
+        var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
+        (function () {
+            var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
+            s1.async = true;
+            s1.src = 'https://embed.tawk.to/68af40d8de9bd819281c8ce4/1j3m9akgi';
+            s1.charset = 'UTF-8';
+            s1.setAttribute('crossorigin', '*');
+            s0.parentNode.insertBefore(s1, s0);
+        })();
+    </script>
+    <!--End of Tawk.to Script-->
     <script>
         AOS.init();
     </script>
