f45c3fc79d6a235b6c829d90833d8a229e6b1164
Your Name <you@example.com>
2025-09-09 04:14:50 -0700

    mustv

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,14 +2,14 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
 
-class User extends Authenticatable
+class User extends Authenticatable implements MustVerifyEmail
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
@@ -63,7 +63,7 @@ class User extends Authenticatable
             ->implode('');
     }
 
-    public function user()
+    public function projects()
     {
         return $this->hasMany(Project::class);
     }
